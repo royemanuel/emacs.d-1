8145c3a947206456d4df4ce84f740fe0ecf45aed
Roy Emanuel <remanuel@alum.mit.edu>
2019-05-05 08:57:41 -0400

    commented out the hard coeded fill-column-indicator

--- snorkel.el
+++ snorkel.el
@@ -414,7 +414,7 @@
 (global-linum-mode t)
 
 ;;Make a column line
-(add-to-list 'load-path "~/.emacs.d/fill-column-indicator-1.83")
+;;(add-to-list 'load-path "~/.emacs.d/fill-column-indicator-1.83")
 (require 'fill-column-indicator)
 (define-globalized-minor-mode
   global-fci-mode fci-mode (lambda() (fci-mode 1)))
